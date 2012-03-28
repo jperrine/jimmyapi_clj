414a2be8d0282c13cc02620b9aa3c375f6dd9fcd
Jimmy Perrine <jimmy.perrine@gmail.com>
2012-03-28 11:31:30 -0400

    some model work

--- src/jimmyapi/models/image.clj
+++ src/jimmyapi/models/image.clj
@@ -11,19 +11,19 @@
   (sql/with-connection (System/getenv "DATABASE_URL")
     (sql/insert-values :images (keys image) (vals image))))
 
-(defn random []
+(defn jimmyapi.models.image/random []
   (sql/with-connection (System/getenv "DATABASE_URL")
     (sql/with-query-results results
-      ["select * from images order by random limit 1"]
-      (into [] results))))
+      ["select * from images order by random() limit 1"]
+      (first (into [] results))))
 
-; (defn find-by-id [id]
-;   (sql/with-connection (System/getenv "DATABASE_URL")
-;     ))
+(defn find-by-id [id]
+  (sql/with-connection (System/getenv "DATABASE_URL")
+    ))
 
-; (defn add-usage [id]
-;   (sql/with-connection (System/getenv "DATABASE_URL")
-;     (let [image])
-;     (sql/update-values :images
-;                        ["id=?" id]
-;                        {:usage }))
+(defn add-usage [id]
+  (sql/with-connection (System/getenv "DATABASE_URL")
+    (let [image (find-by-id id)]
+      (sql/update-values :images
+                         ["id=?" id]
+                         {:usage (+ (:uses image) 1)})))
