d66342466c69954ffa1a6fae6ac29d7ccf1a41c0
Jimmy Perrine <jimmy.perrine@gmail.com>
2012-03-28 11:33:17 -0400

    blerg

--- src/jimmyapi/models/image.clj
+++ src/jimmyapi/models/image.clj
@@ -17,13 +17,13 @@
       ["select * from images order by random() limit 1"]
       (first (into [] results))))
 
-(defn find-by-id [id]
-  (sql/with-connection (System/getenv "DATABASE_URL")
-    ))
+; (defn find-by-id [id]
+;   (sql/with-connection (System/getenv "DATABASE_URL")
+;     ))
 
-(defn add-usage [id]
-  (sql/with-connection (System/getenv "DATABASE_URL")
-    (let [image (find-by-id id)]
-      (sql/update-values :images
-                         ["id=?" id]
-                         {:usage (+ (:uses image) 1)})))
+; (defn add-usage [id]
+;   (sql/with-connection (System/getenv "DATABASE_URL")
+;     (let [image (find-by-id id)]
+;       (sql/update-values :images
+;                          ["id=?" id]
+;                          {:usage (+ (:uses image) 1)}))))
